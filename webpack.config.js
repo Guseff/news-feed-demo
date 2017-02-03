37dfb80cd75e6e17ea842f43ea6c0c07dd361a65
Guseff <vasily.guseff@gmail.com>
2017-02-03 17:07:28 +0300

    environment added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const webpack = require('webpack')
+
 module.exports = {
   entry: "./home",
   output: {
     filename: "build.js",
     library: "home"
   },
-  watch: true,
-  devtool: "source-map",
+
+  watch: NODE_ENV == 'development',
+
+  devtool: NODE_ENV == 'development'? "source-map" : null,
+
+  plugins: [
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV)
+    })
+  ]
 }
--- welcome.js
+++ welcome.js
@@ -1,4 +1,8 @@
 module.exports = function (message) {
 
+  if (process.env.NODE_ENV == 'development') {
+    console.log(message)
+  }
+
   alert(`Welcome, ${message}`)
 }
