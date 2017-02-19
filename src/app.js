31b7e994d52acb5fb79bbe603b88b2f71260b8ef
Guseff <vasily.guseff@gmail.com>
2017-02-19 17:09:12 +0300

    router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Menu from './components/Menu.jsx';
+import App from './conteiners/App.js';
+import Admin from './components/Admin.js';
+import Home from './components/Home.js';
+import About from './components/About.js';
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 ReactDOM.render(
-  <Menu />,
+  <Router history={ browserHistory }>
+    <Route path='/' component={ App }>
+      <IndexRoute component={ Home } />
+      <Route path='about' component={ About } />
+      <Route path='admin' component={ Admin } />
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
