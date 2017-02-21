81e62267cf8610cce8d74c6422c0142ade546da9
Guseff <vasily.guseff@gmail.com>
2017-02-21 12:59:10 +0300

    + mongoose

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var webpackConfig = require('./webpack.config.js')
 var app = express()
 var port = 3000
 
+var ArticleModel    = require('./src/db/mongoose.js').ArticleModel;
+
 app.use(express.static('./public/'));
 app.use(webpackMiddleware(webpack(webpackConfig), {
     // publicPath is required, whereas all other options are optional 
@@ -47,9 +49,115 @@ app.use(webpackMiddleware(webpack(webpackConfig), {
     // Turn off the server-side rendering mode. See Server-Side Rendering part for more info. 
 }));
 
+
 app.get("*", function(req, res) {
   res.sendFile(__dirname + '/public/index.html')
 })
+
+// MongoDB
+
+app.get('/articles', function(req, res) {
+    return ArticleModel.find(function (err, articles) {
+        if (!err) {
+            return res.send(articles);
+        } else {
+            res.statusCode = 500;
+            log.error('Internal error(%d): %s',res.statusCode,err.message);
+            return res.send({ error: 'Server error' });
+        }
+    });
+});
+
+app.post('/articles', function(req, res) {
+    var article = new ArticleModel({
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        images: req.body.images
+    });
+
+    article.save(function (err) {
+        if (!err) {
+            log.info("article created");
+            return res.send({ status: 'OK', article:article });
+        } else {
+            console.log(err);
+            if(err.name == 'ValidationError') {
+                res.statusCode = 400;
+                res.send({ error: 'Validation error' });
+            } else {
+                res.statusCode = 500;
+                res.send({ error: 'Server error' });
+            }
+            log.error('Internal error(%d): %s',res.statusCode,err.message);
+        }
+    });
+});
+
+app.get('/articles/:id', function(req, res) {
+    return ArticleModel.findById(req.params.id, function (err, article) {
+        if(!article) {
+            res.statusCode = 404;
+            return res.send({ error: 'Not found' });
+        }
+        if (!err) {
+            return res.send({ status: 'OK', article:article });
+        } else {
+            res.statusCode = 500;
+            log.error('Internal error(%d): %s',res.statusCode,err.message);
+            return res.send({ error: 'Server error' });
+        }
+    });
+});
+
+app.put('/articles/:id', function (req, res){
+    return ArticleModel.findById(req.params.id, function (err, article) {
+        if(!article) {
+            res.statusCode = 404;
+            return res.send({ error: 'Not found' });
+        }
+
+        article.title = req.body.title;
+        article.description = req.body.description;
+        article.author = req.body.author;
+        article.images = req.body.images;
+        return article.save(function (err) {
+            if (!err) {
+                log.info("article updated");
+                return res.send({ status: 'OK', article:article });
+            } else {
+                if(err.name == 'ValidationError') {
+                    res.statusCode = 400;
+                    res.send({ error: 'Validation error' });
+                } else {
+                    res.statusCode = 500;
+                    res.send({ error: 'Server error' });
+                }
+                log.error('Internal error(%d): %s',res.statusCode,err.message);
+            }
+        });
+    });
+});
+
+app.delete('/articles/:id', function (req, res){
+    return ArticleModel.findById(req.params.id, function (err, article) {
+        if(!article) {
+            res.statusCode = 404;
+            return res.send({ error: 'Not found' });
+        }
+        return article.remove(function (err) {
+            if (!err) {
+                log.info("article removed");
+                return res.send({ status: 'OK' });
+            } else {
+                res.statusCode = 500;
+                log.error('Internal error(%d): %s',res.statusCode,err.message);
+                return res.send({ error: 'Server error' });
+            }
+        });
+    });
+});
+
 
 app.listen(port, function(error) {
   if (error) {
